5ad069e5a415df1acadbf31c8022ef53cda9d161
dv456 <72670501+dv456@users.noreply.github.com>
2022-05-02 23:21:19 +0300

    Update stack.js

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.item = []
+    this.size = 0
+  }
+
+  push(element) {
+    this.item.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.item.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.item[this.stack.length - 1]
   }
 }
 
